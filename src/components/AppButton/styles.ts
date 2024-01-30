d49d5be133fa7210d3ea04062218452b9c1b11df
Felipe Silva Vieira <65823376+fesvieira@users.noreply.github.com>
2024-01-29 21:42:15 -0300

    chore(project): chage to inter font

--- src/components/AppButton/styles.ts
+++ src/components/AppButton/styles.ts
@@ -3,10 +3,5 @@ import styled from "styled-components";
 export const Container = styled.button`
     display: flex;
 
-    & > p {
-        font-size: 16px;
-        font-weight: 600;
-
-        color: ;
-    }
+    
 `
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { GlobalStyles } from "@/styles/global";
 import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
 
+const inter = Inter({ subsets: ["latin"] });
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div className={inter.className}>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </div>
   );
 }
