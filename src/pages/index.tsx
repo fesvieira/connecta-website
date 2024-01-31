3d92a03854cd0a7a69d6af6bb44998e34aecc8d8
Felipe Silva Vieira <65823376+fesvieira@users.noreply.github.com>
2024-01-30 21:59:21 -0300

    feat(ui): make start screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,19 @@
 import { AppButton } from "@/components/AppButton/AppButton";
 import { Container } from "@/styles/Home";
 import { Inter } from "next/font/google";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const aoba = () => {
     console.log("Aoba");
-  }
-  
+  };
+
   return (
     <Container className={inter.className}>
+      <Image src="/icon.png" alt="Connecta" width={200} height={200} />
+
       <h1>Bem vindo ao connecta</h1>
       <p>Forme grupos de 4 palavras!</p>
       <AppButton onClick={aoba}>PLAY</AppButton>
--- src/styles/Home.ts
+++ src/styles/Home.ts
@@ -2,21 +2,21 @@ import styled from "styled-components";
 import { TitleMedium } from "./typography";
 
 export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 
-    align-items: center;
-    justify-content: center;
+  align-items: center;
+  justify-content: center;
 
-    gap: 2rem;
+  gap: 2rem;
 
-    height: 100vh;
+  height: 100vh;
+  width: 100%;
 
-    background-image: linear-gradient(to bottom, #2C488E, #011044);
-    color: white;
+  background-image: linear-gradient(to bottom, #2c488e, #011044);
+  color: white;
 
-    & > h1 {
-        ${TitleMedium}
-    }
-
-`
+  & > h1 {
+    ${TitleMedium}
+  }
+`;
